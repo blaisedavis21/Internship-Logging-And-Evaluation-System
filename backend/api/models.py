4961ebc8e1b7a7476573e3109bc59152604b8525
blaisedavis21 <blaisedavis207@gmail.com>
2026-03-09 16:24:49 +0300

    added abstract user to models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student Intern'),
+        ('workplace_supervisor', 'Workplace Supervisor'),
+        ('academic_supervisor', 'Academic Supervisor'),
+        ('admin', 'Internship Administrator'),
+    ]
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, default='student')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'full_name']
+
+    def __str__(self):
+        return f"{self.full_name} ({self.role})"
