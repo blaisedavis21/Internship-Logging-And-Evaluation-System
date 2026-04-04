482e4ba89894944ebd7bcbaf5af3528a0a058337
blaisedavis21 <blaisedavis207@gmail.com>
2026-04-04 23:39:09 +0300

    ser

--- backend/api/views.py
+++ backend/api/views.py
@@ -245,4 +245,18 @@ def evaluation_list(request):
             serializer.save(evaluator=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
        
