af908235846b3beafd2cc6d7f3ff65aa399931ad
blaisedavis21 <blaisedavis207@gmail.com>
2026-04-09 12:02:26 +0300

    dd

--- frontend/src/pages/supervisor/workplace/Evaluate.jsx
+++ frontend/src/pages/supervisor/workplace/Evaluate.jsx
@@ -89,6 +89,7 @@ const WorkplaceEvaluate = () => {
     return "text-red-400";
   };
 
+  
   const handleScoreChange = (criteria, value, max) => {
     const num = Math.min(Math.max(0, Number(value)), max);
     setCriteriaScores((prev) => ({ ...prev, [criteria]: num }));
